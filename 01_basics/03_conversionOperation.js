81c22fdcbddf19e196bd8b4f5dd8991effe37549
Vivek-Bapodariya <150997429+Vivek-Bapodariya@users.noreply.github.com>
2024-01-30 07:11:06 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "Vivek"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score) 
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0;
+
+
+
+let isLogedIn = "Vivek"
+
+let booleanIsLogedIn = Boolean(isLogedIn)
+
+//console.log(booleanIsLogedIn);
+
+// 1 => true; 0 => false 
+// "" => false
+// "Vivek" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
