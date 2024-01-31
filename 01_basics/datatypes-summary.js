12fed09f63556d1146104bd112e9cf6ba722c4ee
Vivek-Bapodariya <150997429+Vivek-Bapodariya@users.noreply.github.com>
2024-01-31 04:16:21 +0000

    Datatypes-Summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -37,3 +37,39 @@ let myFunction  = function(){
 console.log(typeof heros);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+
+//********************************************************************************************************************** */
+//Stack and Heap memory
+
+//Stack (Primitive), Heap (Non Primitive)
+
+let myYoutubename = "vivekbapodariyadotcom"
+
+let anothername = myYoutubename
+
+anothername = "Chai aur code"
+
+//console.log(myYoutubename);
+//console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+userTwo = userOne
+
+userTwo.email = "vivek@google.com"
+
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
