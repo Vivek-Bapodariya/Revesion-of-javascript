96afcd777443c7667aaadd254406d148c37094f8
Vivek-Bapodariya <150997429+Vivek-Bapodariya@users.noreply.github.com>
2024-02-15 04:24:08 +0000

    for loop

--- 05_iterations/01_one.js
+++ 05_iterations/01_one.js
@@ -3,16 +3,16 @@
 for (let i = 0; i <= 10; i++) {
     const element = i;
     if (i == 5) {
-        // console.log("5 is best number");
+        //  console.log("5 is best number");
     }
-    // console.log(i);
+    //  console.log(i);
 }
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`Outer loop value is ${i}`);
+    // console.log(`Outer loop value is ${i}`);
     for (let j = 1; j <= 10; j++) {
         // console.log(`Inner loop value ${j} and Outer loop value ${i}`);   
-        console.log(i + '*' + j + ' = ' + i*j);     
+        // console.log(i + '*' + j + ' = ' + i*j);     
     }    
 }
 
@@ -21,27 +21,27 @@ let myArray = ["flash", "batman", "superman"]
 
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];    
-    //console.log(element);
+    // console.log(element);
 }
 
 
 //break and continue
 
-// for (let i = 0; i < 20; i++) {
-//     if (i == 5) {
-//         console.log("5 Detected");
-//         break
-//     }
-//     const element = i;    
-//     console.log(`value of i is ${i}`);
-// }
+for (let i = 0; i < 20; i++) {
+    if (i == 5) {
+        console.log("5 Detected");
+        break
+    }
+    const element = i;    
+    console.log(`value of i is ${i}`);
+}
 
 
 
 for (let i = 0; i < 20; i++) {
     if (i == 5) {
         console.log("5 Detected");
-        break
+        continue
     }
     const element = i;    
     console.log(`value of i is ${i}`);
